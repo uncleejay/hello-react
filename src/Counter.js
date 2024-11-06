7a574a7ddf6935ebcb1b041bd7eeda1a335056b4
Joseph Cole-Showers <josephcole-showers@Josephs-MacBook-Pro.local>
2024-11-06 01:31:04 +0000

    initial default counter

---
+++ src/Counter.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+const Counter = () => {
+    // define state
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <p>Count: {count}</p>
+            {/* Update state */}
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
+        </div>
+    )
+}
+
+export default Counter;
